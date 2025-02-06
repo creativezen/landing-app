21a3538f19d2bad3ba7ddac59ca2e3d78736fa2c
Anton Goul'art <tocha2782@gmail.com>
2025-02-06 17:49:55 +0300

    read_achievements to read_section

--- app/crud/achievements.py
+++ app/crud/achievements.py
@@ -1,24 +1,27 @@
 from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import contains_eager
 from sqlalchemy.future import select
 
 
 
-from sections.models import Section, Achievement
+from sections.models import Section, Achievement, models_map
 
 
-async def read_achievements(session, section_id):
+async def read_sections(section_id: int, entity_name: str, session: AsyncSession):
+    model = models_map[entity_name]
+    relation_entity = getattr(Section, entity_name)
     query = (
         select(Section)
-        .join(Achievement, Section.id == Achievement.section_id)
+        .join(model, Section.id == model.section_id)
         # Загружаем achievements
-        .options(contains_eager(Section.achievements))
+        .options(contains_eager(relation_entity))
         .where(Section.id == section_id)
-        .order_by(Section.id, Achievement.order_value)
+        .order_by(Section.id, model.order_value)
     )
     result = await session.execute(query)
-    achievements = result.scalars().first()
-    return achievements
+    relation_data = result.scalars().first()
+    return relation_data
 
 
 # async def read_images(session, entity_name):
