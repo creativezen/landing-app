d1efc7723c7eda4d6d934505f2bc4fead881a27b
Anton Goul'art <tocha2782@gmail.com>
2025-02-07 19:20:29 +0300

    add CardBadge model, card_badge_achievement

--- app/sections/models.py
+++ app/sections/models.py
@@ -1,8 +1,16 @@
 from typing import List
-from sqlalchemy import text, ForeignKey, String, Integer
+from sqlalchemy import text, ForeignKey, String, Integer, Table, Column
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from core.base import Base
+
+
+# Промежуточная таблица для связи многие ко многим
+card_badge_achievement = Table(
+    'card_badge_achievement', Base.metadata,
+    Column('card_badge_id', ForeignKey('badges.id'), primary_key=True),
+    Column('achievement_id', ForeignKey('achievements.id'), primary_key=True)
+)
 
 
 class Section(Base):
@@ -10,15 +18,23 @@ class Section(Base):
 
     title: Mapped[str] = mapped_column(String, nullable=True)
     subtitle: Mapped[str] = mapped_column(String, nullable=True)
-    achievements: Mapped[List["Achievement"]] = relationship(back_populates="section")
+    # Связь с Achievement
+    achievements: Mapped[List["Achievement"]] = relationship(
+        "Achievement", 
+        back_populates="section", 
+    )
     
     
 class CardBadge(Base):
     __tablename__ = "badges"
     
-    title: Mapped[str] = mapped_column(String, nullable=True)
-    card_id: Mapped[int] = mapped_column(Integer, nullable=False)
-    card_table_name: Mapped[int] = mapped_column(Integer, nullable=False)
+    value: Mapped[str] = mapped_column(String, nullable=True)
+    # Связь с Achievement через промежуточную таблицу
+    achievements: Mapped[list["Achievement"]] = relationship(
+        "Achievement",
+        secondary=card_badge_achievement,
+        back_populates="badges"
+    )
     
     
 class Card(Base):
@@ -39,8 +55,17 @@ class Achievement(Card):
     
     table_name: Mapped[str] = mapped_column(String, nullable=True, server_default="achievements")
     section_id: Mapped[int] = mapped_column(ForeignKey('sections.id'))
-    section: Mapped["Section"] = relationship("Section", back_populates="achievements", lazy="joined")
-    
+    section: Mapped["Section"] = relationship(
+        "Section", 
+        back_populates="achievements", 
+        lazy="joined"
+    )
+    # Связь с CardBadge через промежуточную таблицу
+    badges: Mapped[list["CardBadge"]] = relationship(
+        "CardBadge",
+        secondary=card_badge_achievement,
+        back_populates="achievements"
+    )
     
 models_map = {
     'sections': Section,
